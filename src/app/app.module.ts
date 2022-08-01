7a5ccb4f5d9476ea8c4528f2299309f2bb7b7397
AnaliaCasali <100878852+AnaliaCasali@users.noreply.github.com>
2022-08-01 17:39:43 -0300

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
-
+import { FooterComponent } from './componentes/footer/footer.component';
+import { AcercadeComponent } from './componentes/acercade/acercade.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { SkillsComponent } from './componentes/skills/skills.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
-    EncabezadoComponent
+    EncabezadoComponent,
+    FooterComponent,
+    AcercadeComponent,
+    ExperienciaComponent,
+    SkillsComponent,
+    ProyectosComponent,
+    LoginComponent,
+    EducacionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
